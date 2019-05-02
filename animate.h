7d09a9c7543063d4fe828645d047d2118528340f
IlyaBoykoProgr <44905605+IlyaBoykoProgr@users.noreply.github.com>
2019-05-02 19:32:15 +0300

    some changed

--- animate.h
+++ animate.h
@@ -1,14 +1,37 @@
 #include <QWidget>
-void moveWidget(QWidget* moving,int x,int y,long msTime,int repCount=700){
-    float wait=msTime/repCount;
+#include <QThread>
+void moveWidget(QWidget* moving,int x,int y,long msTime){
+    //Qt::WindowFlags flag= moving->windowFlags();
+    //moving->setWindowFlags(Qt::Window);
+    int repCount=msTime;
     if(repCount>x)repCount=x;
     if(repCount>y)repCount=y;
     double xChange=x - moving->x(); xChange/=repCount;
     double yChange=y - moving->y(); yChange/=repCount;
     while(repCount>=0){
         moving->move(moving->x()+xChange,moving->y()+yChange);
-        QThread::msleep(wait);
+	 moving->update();
+        QThread::msleep(1);
         repCount--;
     }
+    //moving->setWindowFlags(flag);
     moving->move(x,y);
+}
+
+void sizeWidget(QWidget *sizing,int w,int h,long msTime){
+   Qt::WindowFlags flag= sizing->windowFlags();
+    sizing->setWindowFlags(Qt::Window);
+    int repCount=msTime/5;
+    if(repCount>w)repCount=w;
+    if(repCount>h)repCount=h;
+    double wChange=w - sizing->width(); wChange/=repCount;
+    double hChange=h - sizing->height(); hChange/=repCount;
+    while(repCount>=0){
+	 sizing->resize(sizing->width()+wChange,sizing->height()+hChange);
+	 sizing->update();
+	 QThread::msleep(5);
+        repCount--;
+    }
+   sizing->setWindowFlags(flag);
+    sizing->resize(w,h);
 }
